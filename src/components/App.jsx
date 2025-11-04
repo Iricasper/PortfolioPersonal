359dd43ea3c3cb0e0d7d8835d783ddd4f087a7b0
Iris <acasper631@g.educaand.es>
2025-11-04 18:48:35 +0100

    Módulo de traducción por google completado

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,8 @@ import "../styles/App.css"
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hola mundo</h1>
+      <p>Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no te cabe parte dellas.Y, viéndole don Quijote de aquella manera, con muestras de tanta tristeza, le dijo: Sábete, Sancho, que no es un hombre más que otro si no hace más que otro. Todas estas borrascas que nos suceden son señales de que presto ha de serenar el tiempo y han de sucedernos bien las cosas; porque no es posible que el mal ni el bien sean durables, y de aquí se sigue que, habiendo durado mucho el mal, el bien está ya cerca. Así que, no debes congojarte por las desgracias que a mí me suceden, pues a ti no</p>
     </div>
   )
 }
--- src/components/TranslateComponent.jsx
+++ src/components/TranslateComponent.jsx
@@ -14,13 +14,13 @@ const TranslateComponent = () => {
         clearInterval(intervalId)
         new window.google.translate.TranslateElement(
           {
-            pageLanguage: "en",
+            pageLanguage: "es",
             autoDisplay: true,
             multilanguagePage: true,
-            // includedLanguages:
-            //   "af,ach,ak,am,ar,az,be,bem,bg,bh,bn,br,bs,ca,chr,ckb,co,crs,cs,cy,da,de,ee,el,en,eo,es,es-419,et,eu,fa,fi,fo,fr,fy,ga,gaa,gd,gl,gn,gu,ha,haw,hi,hr,ht,hu,hy,ia,id,ig,is,it,iw,ja,jw,ka,kg,kk,km,kn,ko,kri,ku,ky,la,lg,ln,lo,loz,lt,lua,lv,mfe,mg,mi,mk,ml,mn,mo,mr,ms,mt,ne,nl,nn,no,nso,ny,nyn,oc,om,or,pa,pcm,pl,ps,pt-BR,pt-PT,qu,rm,rn,ro,ru,rw,sd,sh,si,sk,sl,sn,so,sq,sr,sr-ME,st,su,sv,sw,ta,te,tg,th,ti,tk,tl,tn,to,tr,tt,tum,tw,ug,uk,ur,uz,vi,wo,xh,yi,yo,zh-CN,zh-TW,zu",
+            includedLanguages:
+              "es,en,fr,de,it,pt",
             layout:
-              window.google.translate.TranslateElement.InlineLayout.VERTICAL,
+              window.google.translate.TranslateElement.InlineLayout.SIMPLE,
           },
           googleTranslateRef.current
         )
